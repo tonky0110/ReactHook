c369915e04a52593326caa772c20a3b7c8dec53a
tonky0110 <tonky0110@gmail.com>
2019-07-07 11:50:01 +0900

    class / hooks 기반 소스 비교

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
-import React, {Component, useState} from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [email, setEmail] = useState("");
-  const updateEmail = e => {
-    const { target: { value } } = e;
-    setEmail(value);
-  }
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item+1);
+  const decrementItem = () => setItem(item-1);
   return (
-    <>
-      {count}
-      <button onClick={() => setCount(count+1)}>Increment</button>
-      <button onClick={() => setCount(count-1)}>Decrement</button>
-      <input placehoder="Email" value={email} onChange={updateEmail}/>
-    </>
+    <div>
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>increment</button>
+      <button onClick={decrementItem}>decrement</button>
+    </div>
   )
 }
-export default App;
+
+class AppUgly extends React.Component {
+  state = {
+    item: 1
+  }
+
+  incrementItem = () => this.setState(
+    this.setState(state => {
+      return {
+        item: state.item + 1
+      };
+    })
+  );
+
+  decrementItem = () => this.setState(
+    this.setState(state => {
+      return {
+        item: state.item - 1
+      };
+    })
+  );
+
+  render() {
+    const { item } = this.state;
+    return (
+      <div>
+        <h1>Hello {item}</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={this.incrementItem}>increment</button>
+        <button onClick={this.decrementItem}>decrement</button>
+      </div>
+    );
+  }
+}
+// export default App;
+export default AppUgly;
