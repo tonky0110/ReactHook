755267c80908fd41b8f9c4f336fef098cfe8edb0
tonky0110 <tonky0110@gmail.com>
2019-07-07 12:46:24 +0900

    1.2 useInput

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import React, {useState} from 'react';
+import useInput from './useInput';
+
 
 const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item+1);
-  const decrementItem = () => setItem(item-1);
+  const maxLen = (value) => value.length <= 10 && !value.includes("@");
+  const name = useInput("Mr.", maxLen);
   return (
     <div>
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>increment</button>
-      <button onClick={decrementItem}>decrement</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   )
 }
-
-class AppUgly extends React.Component {
-  state = {
-    item: 1
-  }
-
-  incrementItem = () => this.setState(
-    this.setState(state => {
-      return {
-        item: state.item + 1
-      };
-    })
-  );
-
-  decrementItem = () => this.setState(
-    this.setState(state => {
-      return {
-        item: state.item - 1
-      };
-    })
-  );
-
-  render() {
-    const { item } = this.state;
-    return (
-      <div>
-        <h1>Hello {item}</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.incrementItem}>increment</button>
-        <button onClick={this.decrementItem}>decrement</button>
-      </div>
-    );
-  }
-}
-// export default App;
-export default AppUgly;
+export default App;
